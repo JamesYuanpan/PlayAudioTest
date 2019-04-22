96fce7f259641b7c784507757d92b483f67fccd1
Tom_yuan <1151649069@qq.com>
2019-04-22 11:44:42 +0800

    dddd

---
+++ app/src/main/java/com/yp/playaudiotest/MainActivity.java
@@ -0,0 +1,100 @@
+package com.yp.playaudiotest;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private MediaPlayer mediaPlayer = new MediaPlayer();
+
+    private Button startBtn,pauseBtn,stopBtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        startBtn = (Button) findViewById(R.id.btn_play);
+        pauseBtn = (Button) findViewById(R.id.btn_pause);
+        stopBtn = (Button) findViewById(R.id.btn_stop);
+
+        startBtn.setOnClickListener(this);
+        pauseBtn.setOnClickListener(this);
+        stopBtn.setOnClickListener(this);
+
+        if(ContextCompat.checkSelfPermission(MainActivity.this,Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                 != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},1);
+        }else {
+            initMediaPlayer();
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case 1:
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    initMediaPlayer();
+                }else {
+                    Toast.makeText(this,"拒绝权限将无法使用该程序",Toast.LENGTH_LONG).show();
+                    finish();
+                }
+                break;
+                default:
+        }
+    }
+
+    private void initMediaPlayer() {
+        String path = "https://p.scdn.co/mp3-preview/bdd7d97e2b2b5e30ce3c767ee74795eb581b4f98?cid=774b29d4f13844c495f206cafdad9c86";
+        try {
+            mediaPlayer.setDataSource(path);
+            mediaPlayer.prepare();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_play:
+                if(!mediaPlayer.isPlaying()){
+                    mediaPlayer.start();//开始播放
+                }
+                break;
+            case R.id.btn_pause:
+                if(mediaPlayer.isPlaying()){
+                    mediaPlayer.pause();//暂停播放
+                }
+                break;
+            case R.id.btn_stop:
+                if(mediaPlayer.isPlaying()){
+                    mediaPlayer.reset();
+                    initMediaPlayer();
+                }
+                break;
+                default:
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(mediaPlayer != null){
+            mediaPlayer.start();
+            mediaPlayer.release();
+        }
+    }
+}
